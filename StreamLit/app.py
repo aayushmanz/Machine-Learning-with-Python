d05d748be817a8f2c4fea0b4908ad8804d3b4f8c
Ayush Suthar <sutharayush36@gmail.com>
2026-05-06 16:27:46 +0000

    Add investor details functionality to StreamLit app
    - Implemented a new function to display the last 5 investments of a selected investor.
    - Updated sidebar to include a button for fetching investor details.
    - Enhanced user interface for investor analysis section.

--- StreamLit/app.py
+++ StreamLit/app.py
@@ -3,6 +3,13 @@ import pandas as pd
 
 df = pd.read_csv('/workspaces/Machine-Learning-with-Python/StreamLit/startup_cleaned.csv')
 
+def investor_details(data):
+    st.subheader(investor)
+    investor_data = df[df['investors'].str.contains(investor)].head()[['date', 'startup', 'vertical', 'city', 'round', 'amount']]
+    st.write('Last 5 investments : ')
+    st.dataframe(investor_data)
+
+
 st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select one', ['Overall Anaysis', 'Startup', 'Investor'])
 
@@ -13,7 +20,11 @@ elif option == 'Startup':
     st.title('StartUp Analysis')
     btn1 = st.sidebar.button('Find StartUps Details')
 else:
-     st.sidebar.selectbox('Select startup',set(df['investors'].str.split(',').sum()))
+     investor = st.sidebar.selectbox('Select startup',set(df['investors'].str.split(',').sum()))
      st.title('Investor Analysis')
-     btn1 = st.sidebar.button('Find Invester Details')
+     btn2 = st.sidebar.button('Find Invester Details')
+     if btn2:
+        investor_details(investor)
+        
+
      
