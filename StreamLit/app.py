3062feaab0a7a68f8d87d01e8cabc1e2f18a4733
Ayush Suthar <sutharayush36@gmail.com>
2026-05-06 15:41:06 +0000

    Refactor code structure for improved readability and maintainability

--- StreamLit/app.py
+++ StreamLit/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_csv('/workspaces/Machine-Learning-with-Python/StreamLit/startup_funding.csv')
+df = pd.read_csv('/workspaces/Machine-Learning-with-Python/StreamLit/startup_cleaned.csv')
 
 st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select one', ['Overall Anaysis', 'Startup', 'Investor'])
@@ -9,10 +9,11 @@ option = st.sidebar.selectbox('Select one', ['Overall Anaysis', 'Startup', 'Inve
 if option == 'Overall Anaysis':
     st.title('Overall Anaysis')
 elif option == 'Startup':
-    st.sidebar.selectbox('Select startup',sorted(df['Startup Name'].unique().tolist()))
+    st.sidebar.selectbox('Select startup',sorted(df['startup'].unique().tolist()))
     st.title('StartUp Analysis')
     btn1 = st.sidebar.button('Find StartUps Details')
 else:
-     st.sidebar.selectbox('Select startup',sorted(df['Investors Name'].fillna('Unknown').unique().tolist()))
+     st.sidebar.selectbox('Select startup',set(df['investors'].str.split(',').sum()))
      st.title('Investor Analysis')
      btn1 = st.sidebar.button('Find Invester Details')
+     
