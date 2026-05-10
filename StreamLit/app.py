66b9ab5b20de4c02d0bdcffcbacf0a86b1d550a2
Ayush Suthar <sutharayush36@gmail.com>
2026-05-10 07:53:22 +0000

    Add total funded startups metric to overall analysis in Streamlit app

--- StreamLit/app.py
+++ StreamLit/app.py
@@ -13,9 +13,11 @@ def load_overall_analysis():
     # total invested amount :
     total = round(df['amount'].sum())
     # max amount infused in the startup.
-    max_funding = df.groupby('startup')['amount'].max().sort_values(ascending=False).head(1).values[0]
+    max_funding = round(df.groupby('startup')['amount'].max().sort_values(ascending=False).head(1).values[0])
     # avg ticket size :
     avg_funding = round(df.groupby('startup')['amount'].sum().mean()) 
+    # total funded Startups :
+    total_funded = df['startup'].nunique()
 
     col1, col2, col3, col4 = st.columns(4)
     with col1:
@@ -24,6 +26,8 @@ def load_overall_analysis():
       st.metric('Max', str(max_funding) + ' Cr')
     with col3 :
       st.metric('Avg', str(avg_funding) + ' Cr') 
+    with col4 :
+      st.metric('Funded Startups', str(total_funded))     
 
 def investor_details(data):
     st.subheader(investor)
